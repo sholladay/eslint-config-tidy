fac2b396a681d5bb5e84a8fce1ee33272037c1be
Seth Holladay <me@seth-holladay.com>
2026-04-14 05:02:03 -0400

    Fix typo

--- eslint.js
+++ eslint.js
@@ -1,7 +1,7 @@
 import getXoConfigs from 'eslint-config-xo';
 import tidyConfigs from './index.js';
 
-// Returns a complete config for use with ESLint directly rather via XO
+// Returns a complete config for use with ESLint directly rather than via XO
 const configs = [
     ...getXoConfigs(),
     ...tidyConfigs
